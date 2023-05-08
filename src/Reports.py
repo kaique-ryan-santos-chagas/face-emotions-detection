34ad3889a3b7c612e5bec62309bed5824425d4ff
Kaique Ryan Santos Chagas <kaique.chagas@globalhitss.com.br>
2023-05-08 17:33:31 -0300

    Working in report generate

--- src/Reports.py
+++ src/Reports.py
@@ -12,8 +12,6 @@ from jinja2 import Template
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
-from email.mime.base import MIMEBase
-from email import encoders
 from dotenv import load_dotenv
 
 class Reports: 
