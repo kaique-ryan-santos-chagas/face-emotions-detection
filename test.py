324dfa90522c965e5740859c175455a826c6a6f1
Kaique Ryan Santos Chagas <kaique.chagas@globalhitss.com.br>
2023-05-24 14:28:12 -0300

    Added file to test send e-mail

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from src.Reports import Reports
 
-report = Reports('dominiquegomes4@gmail.com', 'react_8eeaced1f2e0dfc753cc11e63b5abc12')
+report = Reports('rafadantasdasilva54@gmail.com', 'react_2f5ebb0642cfa2a25af21fac655b30eb')
 
 report.send_email()
